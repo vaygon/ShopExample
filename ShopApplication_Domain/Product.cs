dd0812759f8e9660238e5de6c9e7832686b99c10
vaygon <leemargiotta@gmail.com>
2014-04-22 20:22:43 +0100

    Added Repository, Entity Models and Controller

---
+++ ShopApplication_Domain/Product.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShopApplication_Domain
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Sku { get; set; }
+
+        public virtual List<Price> Prices { get; set; } 
+
+    }
+
+    public class Price
+    {
+        public int Id { get; set; }
+        public double Cost { get; set; }
+        public int ProductId { get; set; }
+        public DateTime Date { get; set; }
+
+        public virtual Product Product { get; set; }
+    }
+}
