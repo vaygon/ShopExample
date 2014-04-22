3f48afbc5862a3e6b9e5f4ea8319169cdbaa36a1
vaygon <leemargiotta@gmail.com>
2014-04-22 19:09:51 +0100

    First Commit Setup Inital Project

---
+++ ShopApplication/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ShopApplication.MvcApplication" Language="C#" %>
---
+++ ShopApplication/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ShopApplication.Startup))]
+namespace ShopApplication
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
